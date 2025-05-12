a8db83529bf889136eacee8d6ed8046352f76afb
EmmerichFrog <a@a>
2025-05-12 20:19:09 +0200

    increase queue dimension

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,14 @@ void setup() {
   TaskHandle_t CommTask;
   QueueHandle_t queue;
 
-  /* uncommentm for formatting ffat first time
+  /* uncomment for formatting ffat first time
   delay(10000);
   Serial.println("formatting");
   bool sts = FFat.format();
   Serial.print("Done");
   Serial.println(sts);
   FFat.begin(); */
-  queue = xQueueCreate(1, sizeof(std::tuple<uint16_t, uint16_t, float, float>));
+  queue = xQueueCreate(2, sizeof(std::tuple<uint16_t, uint16_t, float, float>));
   setCpuFrequencyMhz(80);
   xTaskCreate(reading, "Reading", 10000, (void*)queue, 0, &SensorTask);
   xTaskCreate(sending, "MQTT", 20000, (void*)queue, 0, &CommTask);
